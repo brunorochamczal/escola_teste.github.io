ea2c6ff913816cdd59ca22c5a466a13ee1f8c90f
brunorochamczal <brunorocha81@gmail.com>
2025-12-27 00:02:35 -0300

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,1 @@
-import { defineConfig } from "vite";
-import path from "path";
 
-export default defineConfig({
-  root: path.resolve(__dirname, "client"),
-  base: './', 
-  build: {
-    outDir: path.resolve(__dirname, "dist"),
-    emptyOutDir: true,
-  }
-});
